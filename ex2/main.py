205b7dc2d74695eb4d9f2d5da2fe1a868f188672
Martin Glatzle <mglatzle@mpa-garching.mpg.de>
2018-05-18 12:14:01 +0200

    style: fixes

--- ex2/main.py
+++ ex2/main.py
@@ -9,14 +9,14 @@ import logging
 pypath,libpath = os.path.split(os.path.split(os.path.realpath(sys.argv[0]))[0])
 sys.path.append(pypath)
 
-#import 
+#import
 from ex2.operations import plus
 
 #2.2.5 OD code the features in the new branches : remember to add import of your features
 
 
 #print python version
-print("PYTHON VERSION : " +str(sys.version_info[:3])) 
+print("PYTHON VERSION : " +str(sys.version_info[:3]))
 
 print("Calculator software")
 
@@ -27,5 +27,5 @@ b = 5
 
 #2.1.10 Tl and OD change the same line on local repos
 c = plus.operation(a,b)
- 
-print("RESULT = "+str(c)) 
+
+print("RESULT = "+str(c))
